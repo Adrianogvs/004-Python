c698db3ee435c3484773e720439cc4aa39ec9411
Adrianogvs <dataengineer@adrianogvs.com.br>
2025-02-11 10:22:06 -0300

    feat: Aula 13 adicionada.

--- aula013.py
+++ aula013.py
@@ -1,9 +1,15 @@
-primeiro_valor = input('Digite um valor: ')
-segundo_valor = input('Digite outro valor: ')
+# Solicita ao usuário a entrada de dois valores numéricos
+try:
+    primeiro_valor = float(input('Digite o primeiro valor: '))
+    segundo_valor = float(input('Digite o segundo valor: '))
 
-if segundo_valor > primeiro_valor:
-    print(f'Segundo valor {segundo_valor} é maior que o primeiro {primeiro_valor}.')
-elif primeiro_valor > segundo_valor:
-    print(f'Primeiro valor {primeiro_valor} é maior que o segundo valor {segundo_valor}')
-else:
-    print(f'Os dois valores são iguais.')
+    # Compara os valores e exibe o resultado
+    if segundo_valor > primeiro_valor:
+        print(f'O segundo valor ({segundo_valor}) é maior que o primeiro valor ({primeiro_valor}).')
+    elif primeiro_valor > segundo_valor:
+        print(f'O primeiro valor ({primeiro_valor}) é maior que o segundo valor ({segundo_valor}).')
+    else:
+        print('Os dois valores são iguais.')
+
+except ValueError:
+    print('Erro: Por favor, digite apenas números válidos.')
