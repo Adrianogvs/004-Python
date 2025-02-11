2561dad822074862252f7d889805059a9f8f46e7
Adrianogvs <dataengineer@adrianogvs.com.br>
2025-02-11 08:53:34 -0300

    docs: Curso Python

---
+++ aula012.py
@@ -0,0 +1,18 @@
+# if / elif      / else
+# se / se não se / se não
+
+condicao1 = False
+condicao2 = False
+condicao3 = False
+condicao4 = False
+
+if condicao1:
+    print('Código para a condição 1')
+elif condicao2:
+    print('Condigo para a condição 2')
+elif condicao3:
+    print('Condigo para a condição 3')
+elif condicao4:
+    print('Condigo para a condição 4')
+else:
+    print('Nenhuma condição foi satisfeita')    
