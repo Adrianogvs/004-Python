9fd20054724499a85810ee3ba1bbc88888c2d235
Adrianogvs <dataengineer@adrianogvs.com.br>
2025-02-28 12:34:46 -0300

    feat: Exercicios.

---
+++ Suzano-Python-Developer/02-Sintaxe-Basica/05-Projeto-Sistema-Bancario/09.py
@@ -0,0 +1,14 @@
+"""
+Parte 3: Avançado
+3.1 Programação Orientada a Objetos
+Crie uma classe Pessoa com os seguintes atributos:
+
+nome
+idade
+E os seguintes métodos:
+
+apresentar() que imprime uma mensagem com o nome e a idade da pessoa.
+eh_maior_de_idade() que retorna True se a pessoa for maior de idade e False caso contrário.
+
+"""
+
