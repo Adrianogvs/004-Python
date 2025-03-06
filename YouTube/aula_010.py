8fe323c89b0d1121eee884a0714e7512427a89c2
Adrianogvs <dataengineer@adrianogvs.com.br>
2025-03-06 19:37:23 -0300

    feat: Curso em Video.

--- YouTube/aula_010.py
+++ YouTube/aula_010.py
@@ -5,7 +5,7 @@ print(f'\n{5*"=-="} Uso de For ' f'{5*"=-="}\n')
 
 for i in range(0,11):
     valor = i * numero
-    print(f'{numero} X {i:02} = {valor:03}.')
+    print(f'{numero} X {i:2} = {valor:3}.')
 
 print(f'\n{5*"=-="} Uso de While ' f'{5*"=-="}\n')
 
@@ -13,5 +13,5 @@ i = 0
 
 while i < 11:
     valor = i * numero
-    print(f'{numero} X {i:02} = {valor:03}.')
+    print(f'{numero} X {i:2} = {valor:3}.')
     i += 1
